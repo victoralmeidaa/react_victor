626ad25bfd9e42402c938efd68d66dfdc7e6988c
Victor <victordealmeida5@gmail.com>
2023-07-23 19:07:12 -0300

    link social

--- src/components/SocialNetworks.jsx
+++ src/components/SocialNetworks.jsx
@@ -3,16 +3,17 @@ import { FaLinkedinIn, FaGithub, FaInstagram } from "react-icons/fa"
 import "../styles/components/socialnetworks.sass"
 
 const socialNetworks = [
-    { name: "linkedin", icon: <FaLinkedinIn/>},
-    { name: "github", icon: <FaGithub/>},
-    { name: "instagram", icon: <FaInstagram/>},
+    { name: "linkedin", href: "https://www.linkedin.com/in/victalmeida/" ,icon: <FaLinkedinIn/>},
+    { name: "github", href: "https://github.com/victoralmeidaa" ,icon: <FaGithub/>},
+    { name: "instagram", href: "https://www.instagram.com/vict_5/" ,icon: <FaInstagram/>},
 ];
 
 const SocialNetworks = () => {
     return (
     <section id="social-networks">
+
         {socialNetworks.map((network) => (
-            <a href="#" className="social-btn" id={network.name} key={network.name}>
+            <a href={network.href} className="social-btn" id={network.name} key={network.name}>
                 { network.icon } 
             </a>
         ))}
